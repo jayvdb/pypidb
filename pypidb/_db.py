271275a355148baa1705e7c35c7f03240416acb7
John Vandenberg <jayvdb@gmail.com>
2020-03-25 07:31:44 +0700

    reverse_mappings: Add old ujson

--- pypidb/_db.py
+++ pypidb/_db.py
@@ -24,6 +24,7 @@ reverse_mappings = {
     "https://opendev.org/openstack-dev/pbr": "pbr",
     "https://github.com/vmalloc/flux": "flux",
     "https://github.com/getslash/flux": "flux",
+    "https://github.com/esnme/ultrajson": "ujson",
 }
 
 multipackage_repos = {
