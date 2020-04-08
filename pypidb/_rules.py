jayvdb/pypidb

commit 80ba80b9e6bb03f5e2d0b158c3f5df385c06e572

SCM picker: Improve freedesktop SCM
Fixes pyxdg, dbus-python, and three others hosted by
gitlab.freedesktop.org

Fixes https://github.com/jayvdb/pypidb/issues/41

pypidb/_rules.py
@@ -723,6 +723,7 @@ rules.from_set(
         Rule("webunit", ["docutils"], expect_none=True),
         Rule("wmi", ["pywin32"], expect_none=True),
         Rule("wordaxe", ignore_bad_metadata=True),
+        Rule("xpyb", ignore_bad_metadata=True),
         Rule("xml2rfc", ["weasyprint"]),
         Rule("xmltramp", ignore_bad_metadata=True),
         Rule(
@@ -871,6 +872,8 @@ def _find_named_repo(name, emails=None):
             sf_project_name, list_type = address.rsplit("-", 1)
             if list_type in ["bugs", "discuss"]:
                 return "https://sourceforge.net/projects/{}".format(sf_project_name)
+        elif domain == "lists.freedesktop.org":
+            return "https://gitlab.freedesktop.org/{}/{}".format(address, name)
 
         email_rules = _email_matches.get(email, [])
         if isinstance(email_rules, str):

pypidb/_scm_url_cleaner.py
@@ -168,16 +168,22 @@ def _hostname_three_paths(url):
     return "https://{}/{}/{}/{}".format(p.netloc, path1, path2, path3)
 
 
-def _subdomain_to_path(url, hostname=None, prefix=None, exclude=[]):
+def _subdomain_to_path(url, hostname=None, prefix=None, exclude=[], append_part=None):
     p = urlsplit(url)
     subdomain, _, domain = p.netloc.partition(".")
     if not hostname:
         hostname = domain
     if subdomain in exclude:
         return
+    suffix = ""
+    if append_part:
+       if p.path[0] != "/":
+           return
+       parts = p.path[1:].split("/")
+       suffix = "/" + parts[append_part - 1]
     if prefix:
-        return "https://{}/{}{}".format(hostname, prefix, subdomain)
-    return "https://{}/{}".format(hostname, subdomain)  # pragma: no cover
+        return "https://{}/{}{}{}".format(hostname, prefix, subdomain, suffix)
+    return "https://{}/{}{}".format(hostname, subdomain, suffix)  # pragma: no cover
 
 
 def _ghbtns(url):
@@ -669,6 +675,13 @@ class SCMURLCleaner(object):
         "foss.heptapod.net": _gitlab,
         "code.qt.io/cgit/": _strip_after_dot_git,
         "gitlab.freedesktop.org": _gitlab,
+        "*.freedesktop.org/releases/": partial(
+            _subdomain_to_path,
+            hostname="gitlab.freedesktop.org",
+            append_part=2,
+        ),  # dbus-python
+        "cgit.freedesktop.org/wiki/": _reject,
+        "cgit.freedesktop.org": _hostname_two_paths,
         "git.tuxfamily.org/gitroot": _reject,
         "git.tuxfamily.org": _strip_after_dot_git,
         "salsa.debian.org": _all,

pypidb/_similarity.py
@@ -174,6 +174,14 @@ def _get_shortest(urls):
 def _get_prefered(urls):
     orig_urls = urls
 
+    freedesktop_urls = [url for url in urls if "gitlab.freedesktop.org" in url]
+    if len(freedesktop_urls) == 1:
+        return freedesktop_urls[0]
+
+    freedesktop_urls = [url for url in urls if "cgit.freedesktop.org" in url]
+    if len(freedesktop_urls) == 1:
+        return freedesktop_urls[0]
+
     opendev_urls = [url for url in urls if "opendev.org" in url]
     if len(opendev_urls) == 1:
         return opendev_urls[0]

tests/test_frameworks.py
@@ -834,6 +834,31 @@ class TestQt(_TestBase):
         )
 
 
+class TestFreedesktop(_TestBase):
+    def test_pyxdg(self):
+        # https://github.com/jayvdb/pypidb/issues/41 should be
+        # http://cgit.freedesktop.org/xdg/pyxdg/
+        url = self.converter.get_vcs("pyxdg")
+        #self.assertInsensitiveEqual(url, "https://github.com/takluyver/pyxdg")
+        self.assertInsensitiveEqual(url, "https://gitlab.freedesktop.org/xdg/pyxdg")
+
+    def test_dbus(self):
+        url = self.converter.get_vcs("dbus-python")
+        self.assertInsensitiveEqual(url, "https://gitlab.freedesktop.org/dbus/dbus-python")
+
+    def test_hid_tools(self):
+        url = self.converter.get_vcs("hid-tools")
+        self.assertInsensitiveEqual(url, "https://gitlab.freedesktop.org/libevdev/hid-tools")
+
+    def test_libevdev(self):
+        url = self.converter.get_vcs("libevdev")
+        self.assertInsensitiveEqual(url, "https://gitlab.freedesktop.org/libevdev/python-libevdev")
+
+    def test_xpyb(self):
+        url = self.converter.get_vcs("xpyb")
+        self.assertInsensitiveEqual(url, "https://gitlab.freedesktop.org/xcb/xpyb")
+
+
 class TestMaxmind(_TestBase):
     def test_GeoIP(self):
         url = self.converter.get_vcs("GeoIP")

tests/test_pypi.py
@@ -460,10 +460,6 @@ class TestPyPiMisc(_TestBase):
             url, "https://github.com/ros-infrastructure/catkin_pkg"
         )
 
-    def _test_pyxdg(self):
-        url = self.converter.get_vcs("pyxdg")
-        self.assertInsensitiveEqual(url, "https://github.com/takluyver/pyxdg")
-
     def test_imagecodecs(self):
         url = self.converter.get_vcs("imagecodecs")
         self.assertInsensitiveEqual(url, "https://github.com/cgohlke/imagecodecs")

commit 58863be8ab17a88f4e9dcffe74685194b53a7d4d

rules: Add rules for dbus-signature projects
Avoids incorrect results introduced in 80ba80b

Fixes https://github.com/jayvdb/pypidb/issues/97

pypidb/_rules.py
@@ -324,9 +324,7 @@ rules.from_set(
         ),
         Rule(
             "dbus-signature-pyparsing",
-            ["hypothesis"],
-            ignore_urls=["hypothesis.works"],
-            expect_none=True,
+            patch="https://github.com/stratis-storage/dbus-signature-pyparsing/pull/13",
         ),
         Rule("debian", ignore_bad_metadata=True),
         Rule(
@@ -446,7 +444,7 @@ rules.from_set(
         Rule("gsutil", ignore_urls=["cloud.google.com/storage/docs/gsutil_install"]),
         Rule("hacking", ["pep8"]),
         Rule("haproxyctl", ignore_bad_metadata=True),
-        Rule("hs-dbus-signature", ["hypothesis"], expect_none=True),
+        Rule("hs-dbus-signature", patch="https://github.com/stratis-storage/hs-dbus-signature/pull/31"),
         Rule("hypothesis-pytest", ["hypothesis"], expect_none=True),
         Rule("imagecodecs", link_extract=get_html_hrefs),
         Rule("imreg", link_extract=get_html_hrefs),

tests/data.py
@@ -49,6 +49,7 @@ exact_metadata = {
     "Contextual": "https://github.com/peak-legacy/contextual",
     "couchbase": "https://github.com/couchbase/couchbase-python-client",
     "cssutils": "https://bitbucket.org/cthedot/cssutils",
+    "dbus-signature-pyparsing": "https://github.com/stratis-storage/dbus-signature-pyparsing",
     "ddtrace": "https://github.com/DataDog/dd-trace-py",
     "DecoratorTools": "https://github.com/peak-legacy/decoratortools",
     "delegator-py": "https://github.com/kennethreitz/delegator",
@@ -97,6 +98,7 @@ exact_metadata = {
     "hacs-frontend": "https://github.com/hacs/frontend",
     "hdfs": "https://github.com/mtth/hdfs",
     "honeycomb-beeline": "https://github.com/honeycombio/beeline-python",
+    "hs-dbus-signature": "https://github.com/stratis-storage/hs-dbus-signature",
     "ibm-cos-sdk-core": "https://github.com/ibm/ibm-cos-sdk-python-core",
     "ibm-cos-sdk-s3transfer": "https://github.com/ibm/ibm-cos-sdk-python-s3transfer",
     "ibm-db": "https://github.com/ibmdb/python-ibmdb",
@@ -861,7 +863,6 @@ bad_metadata = [
     "dateutils",  # todo
     "dbf",
     "dbus-python",
-    "dbus-signature-pyparsing",
     "dci-utils",
     "demjson",
     "dependency_management",  # recheck
@@ -912,7 +913,6 @@ bad_metadata = [
     "google",
     "google-cloud-dataflow",
     "graph-tool",  # https://github.com/count0/graph-tool
-    "hs-dbus-signature",
     "hurry-filesize",
     "hypothesis-pytest",  # now part of https://github.com/DRMacIver/hypothesis
     "IGitt",

tests/test_exceptions.py
@@ -192,10 +192,6 @@ class TestExceptions(_TestBase):
         with self.assertRaises(PackageWithoutUrls):
             self.converter.get_vcs("awsebcli")
 
-    def test_hs_dbus_signature(self):
-        with self.assertRaises(PackageWithoutUrls):
-            self.converter.get_vcs("hs-dbus-signature")
-
     def test_github_issues_tools(self):
         with self.assertRaises(IncompletePackageMetadata):
             self.converter.get_vcs("github-issues-tools")

tests/test_fedora.py
@@ -60,7 +60,6 @@ class TestFedora(_TestBase):
         "coverage_pth",
         "cvxopt",
         "dbf",
-        "dbus-signature-pyparsing",
         "demjson",
         "dialog",
         "digitalocean",  # version 0
@@ -69,7 +68,6 @@ class TestFedora(_TestBase):
         "fiat",
         "geoip",
         "graph-tool",
-        "hs-dbus-signature",
         "interfile",
         "justbases",
         "keystoneclient",  # version 0